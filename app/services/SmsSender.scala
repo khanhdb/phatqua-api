855d4c312cafa6f7ea378e117622f8ee083289d0
Bao Khanh Dinh <khanhdb@sandinh.net>
2021-10-14 17:57:44 +0700

    implement Sms Sender

--- app/services/SmsSender.scala
+++ app/services/SmsSender.scala
@@ -1,6 +1,7 @@
 package services
 
 import play.api.Logger
+import play.api.libs.json.Json
 import play.api.libs.ws.WSClient
 
 import javax.inject.Inject
@@ -12,6 +13,11 @@ class SmsSender @Inject() (wsClient: WSClient){
 
   def send(phone: String, content: String): Future[Int] = {
     logger.debug(s"sent : $content to number ${phone} ")
+    val url = "https://chanphom.com/api-c3/user/fix-mission/send-sms"
+    wsClient.url(url).post(Json.obj(
+      content -> content,
+      phone -> phone
+    ))
     Future.successful(0)
   }
 }
